03d462b93e3aacbc94fd49830dfdcb70ca89b241
pengxiangnan <1429648501@qq.com>
2018-04-17 23:28:12 +0800

    1.鼠标点击绘制一个点,点的颜色根据象限来确定

--- WebGL编程指南/ch2/ColoredPoints.js
+++ WebGL编程指南/ch2/ColoredPoints.js
@@ -1,17 +1,18 @@
 // ClickedPints.js (c) 2012 matsuda
 // 顶点着色器
 var VSHADER_SOURCE =
-  'attribute vec4 a_Position;\n' +
-  'void main() {\n' +
-  '  gl_Position = a_Position;\n' +
-  '  gl_PointSize = 10.0;\n' +
-  '}\n';
+    'attribute vec4 a_Position;\n' +
+    'void main() {\n' +
+    '  gl_Position = a_Position;\n' +
+    '  gl_PointSize = 10.0;\n' +
+    '}\n';
 
 // 片元着色器
 var FSHADER_SOURCE =
-  'void main() {\n' +
-  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
-  '}\n';
+    'uniform vec4 u_FragColor;\n' +
+    'void main() {\n' +
+    '  gl_FragColor = u_FragColor;\n' +
+    '}\n';
 
 function main() {
     // Retrieve <canvas> element
@@ -37,9 +38,16 @@ function main() {
         return;
     }
 
+    // Get the storage location of u_FragColor
+    var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+    if (u_FragColor < 0) {
+        console.log('Failed to get the storage location of u_FragColor');
+        return;
+    }
+
     // Register function (event handler) to be called on a mouse press
     canvas.onmousedown = function(ev){
-        click(ev, gl, canvas, a_Position);
+        click(ev, gl, canvas, a_Position, u_FragColor);
     };
 
     // Specify the color for clearing <canvas>
@@ -50,7 +58,8 @@ function main() {
 }
 
 var g_points = []; // The array for the position of a mouse press
-function click(ev, gl, canvas, a_Position) {
+var g_color = []; // 存储点颜色的数组
+function click(ev, gl, canvas, a_Position, u_FragColor) {
     var x = ev.clientX; // x coordinate of a mouse pointer
     var y = ev.clientY; // y coordinate of a mouse pointer
     var rect = ev.target.getBoundingClientRect() ;
@@ -61,6 +70,15 @@ function click(ev, gl, canvas, a_Position) {
     g_points.push(x);
     g_points.push(y);
 
+    // 将点的颜色储存到数组中
+    if (x >= 0.0 && y >= 0.0) {      // First quadrant
+        g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
+    } else if (x < 0.0 && y < 0.0) { // Third quadrant
+        g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
+    } else {                         // Others
+        g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
+    }
+
     // Clear <canvas>
     gl.clear(gl.COLOR_BUFFER_BIT);
 
@@ -68,6 +86,8 @@ function click(ev, gl, canvas, a_Position) {
     for(var i = 0; i < len; i += 2) {
         // Pass the position of a point to a_Position variable
         gl.vertexAttrib3f(a_Position, g_points[i], g_points[i+1], 0.0);
+
+        gl.uniform4f(u_FragColor, g_colors[i/2][0], g_colors[i/2][1], g_colors[i/2][2],g_colors[i/2][3]);
 
         // Draw
         gl.drawArrays(gl.POINTS, 0, 1);

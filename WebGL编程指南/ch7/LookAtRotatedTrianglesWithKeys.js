c1e6f6c9ad5436320a1face34b800ad0a391696b
15707901327 <1429648501@qq.com>
2018-06-22 16:51:59 +0800

    1.视点

---
+++ WebGL编程指南/ch7/LookAtRotatedTrianglesWithKeys.js
@@ -0,0 +1,151 @@
+// LookAtRotatedTriangles.js
+// Vertex shader program 顶点着色器
+var VSHADER_SOURCE =
+  'attribute vec4 a_Position;\n' +
+  'attribute vec4 a_Color;\n' +
+  'uniform mat4 u_ViewMatrix;\n' +
+  'varying vec4 v_Color;\n' +
+  'void main() {\n' +
+  '  gl_Position =  u_ViewMatrix * a_Position;\n' +
+  '  v_Color = a_Color;\n' +
+  '}\n';
+
+// 片元着色器
+var FSHADER_SOURCE =
+  '#ifdef GL_ES\n' +
+  'precision mediump float;\n' +
+  '#endif\n' +
+  'varying vec4 v_Color;\n' +
+  'void main() {\n' +
+  ' gl_FragColor = v_Color;\n' +
+  '}\n';
+
+function main() {
+  var canvas = document.getElementById('webgl');
+
+  // Get the rendering context for WebGL
+  var gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+
+  // Initialize shaders
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+    return;
+  }
+
+  // 设置顶点的位置
+  var n = initVertexBuffers(gl);
+  if (n < 0) {
+    console.log('Failed to set the vertex information');
+    return;
+  }
+
+  // 设置<canvas>背景色
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  /** 获取变量的存储地址 **/
+  var u_ViewMatrix = gl.getUniformLocation(gl.program, 'u_ViewMatrix');
+  var u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+
+  /** 设置视点、视线、和上方向 **/
+  var viewMatrix = new Matrix4();
+  document.onkeydown = function (ev) {
+    keydown(ev, gl, n, u_ViewMatrix, viewMatrix);
+  };
+
+  /** 计算旋转矩阵 **/
+  var modelMatrix = new Matrix4();
+  modelMatrix.setRotate(-90, 0, 0, 1);
+
+  /** 将视图矩阵传递给变量 **/
+  gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
+
+  draw(gl, n, u_ViewMatrix, viewMatrix);
+}
+
+/**
+ * 创建顶点缓存区对象，并将多个顶点的数据保存在缓存区中，然后将缓存区传递给着色器。
+ * @param gl：上下文
+ * @returns {number}：顶点的个数
+ */
+function initVertexBuffers(gl) {
+  var verticesColors = new Float32Array([
+    // 顶点和颜色坐标
+    0.0, 0.5, -0.4, 0.4, 1.0, 0.4, // 绿色三角形在最后
+    -0.5, -0.5, -0.4, 0.4, 1.0, 0.4,
+    0.5, -0.5, -0.4, 1.0, 0.4, 0.4,
+
+    0.5, 0.4, -0.2, 1.0, 0.4, 0.4, // 黄色三角形在中间
+    -0.5, 0.4, -0.2, 1.0, 1.0, 0.4,
+    0.0, -0.6, -0.2, 1.0, 1.0, 0.4,
+
+    0.0, 0.5, 0.0, 0.4, 0.4, 1.0,  // 蓝色三角形在最前面
+    -0.5, -0.5, 0.0, 0.4, 0.4, 1.0,
+    0.5, -0.5, 0.0, 1.0, 0.4, 0.4,
+  ]);
+  var n = 9; // 顶点数目
+
+  // 创建缓存区对象
+  var vertexColorBuffer = gl.createBuffer();
+  if (!vertexColorBuffer) {
+    console.log('Failed to create the buffer object');
+    return false;
+  }
+
+  // 将顶点坐标和纹理坐标写入缓存区对象
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
+
+  var FSIZE = verticesColors.BYTES_PER_ELEMENT;
+
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return -1;
+  }
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 5, 0);
+  gl.enableVertexAttribArray(a_Position);
+
+  // 将纹理坐标分派给a_TexCoord，并开启它
+  var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+  if (a_Color < 0) {
+    console.log('Failed to get the storage location of a_Color');
+    return -1;
+  }
+  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2);
+  gl.enableVertexAttribArray(a_Color);  // Enable the assignment of the buffer object
+
+  // Unbind the buffer object
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  return n;
+}
+
+var g_eyeX = 0.20, g_eyeY = 0.25, g_eyeZ = 0.25;// 视点
+function keydown(ev, gl, n, u_ViewMatrix, viewMatrix) {
+  if (ev.keyCode === 39) { // 按下右键
+    g_eyeX += 0.01;
+  } else {
+    if (ev.keyCode === 37) { // 按下左键
+      g_eyeX -= 0.01;
+    } else {
+      return; // 按下其他键
+    }
+  }
+
+  draw(gl, n, u_ViewMatrix, viewMatrix);
+}
+
+function draw(gl, n, u_ViewMatrix, viewMatrix) {
+  viewMatrix.setLookAt(g_eyeX, g_eyeY, g_eyeZ, 0, 0, 0, 0, 1, 0);
+
+  gl.uniformMatrix4fv(u_ViewMatrix, false, viewMatrix.elements);
+
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // 绘制图形
+  gl.drawArrays(gl.TRIANGLES, 0, n);
+}
---
+++ WebGL编程指南/ch7/OrthoView.js
@@ -0,0 +1,153 @@
+// LookAtRotatedTriangles.js
+// Vertex shader program 顶点着色器
+var VSHADER_SOURCE =
+  'attribute vec4 a_Position;\n' +
+  'attribute vec4 a_Color;\n' +
+  'uniform mat4 u_ProjMatrix;\n' +
+  'varying vec4 v_Color;\n' +
+  'void main() {\n' +
+  '  gl_Position =  u_ProjMatrix * a_Position;\n' +
+  '  v_Color = a_Color;\n' +
+  '}\n';
+
+// 片元着色器
+var FSHADER_SOURCE =
+  '#ifdef GL_ES\n' +
+  'precision mediump float;\n' +
+  '#endif\n' +
+  'varying vec4 v_Color;\n' +
+  'void main() {\n' +
+  ' gl_FragColor = v_Color;\n' +
+  '}\n';
+
+function main() {
+  var canvas = document.getElementById('webgl');
+  var nf = document.getElementById('nearFar');
+
+  // Get the rendering context for WebGL
+  var gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+
+  // Initialize shaders
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+    return;
+  }
+
+  // 设置顶点的位置
+  var n = initVertexBuffers(gl);
+  if (n < 0) {
+    console.log('Failed to set the vertex information');
+    return;
+  }
+
+  // 设置<canvas>背景色
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  /** 获取变量的存储地址 **/
+  var u_ProjMatrix = gl.getUniformLocation(gl.program, 'u_ProjMatrix');
+
+  /** 设置视点、视线、和上方向 **/
+  var projMatrix = new Matrix4();
+
+  document.onkeydown = function (ev) {
+    keydown(ev, gl, n, u_ProjMatrix, projMatrix, nf);
+  };
+
+  draw(gl, n, u_ProjMatrix, projMatrix, nf);
+}
+
+/**
+ * 创建顶点缓存区对象，并将多个顶点的数据保存在缓存区中，然后将缓存区传递给着色器。
+ * @param gl：上下文
+ * @returns {number}：顶点的个数
+ */
+function initVertexBuffers(gl) {
+  var verticesColors = new Float32Array([
+    // 顶点和颜色坐标
+    0.0, 0.5, -0.4, 0.4, 1.0, 0.4, // 绿色三角形在最后
+    -0.5, -0.5, -0.4, 0.4, 1.0, 0.4,
+    0.5, -0.5, -0.4, 1.0, 0.4, 0.4,
+
+    0.5, 0.4, -0.2, 1.0, 0.4, 0.4, // 黄色三角形在中间
+    -0.5, 0.4, -0.2, 1.0, 1.0, 0.4,
+    0.0, -0.6, -0.2, 1.0, 1.0, 0.4,
+
+    0.0, 0.5, 0.0, 0.4, 0.4, 1.0,  // 蓝色三角形在最前面
+    -0.5, -0.5, 0.0, 0.4, 0.4, 1.0,
+    0.5, -0.5, 0.0, 1.0, 0.4, 0.4,
+  ]);
+  var n = 9; // 顶点数目
+
+  // 创建缓存区对象
+  var vertexColorBuffer = gl.createBuffer();
+  if (!vertexColorBuffer) {
+    console.log('Failed to create the buffer object');
+    return false;
+  }
+
+  // 将顶点坐标和纹理坐标写入缓存区对象
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
+
+  var FSIZE = verticesColors.BYTES_PER_ELEMENT;
+
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return -1;
+  }
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 5, 0);
+  gl.enableVertexAttribArray(a_Position);
+
+  // 将纹理坐标分派给a_TexCoord，并开启它
+  var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+  if (a_Color < 0) {
+    console.log('Failed to get the storage location of a_Color');
+    return -1;
+  }
+  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2);
+  gl.enableVertexAttribArray(a_Color);  // Enable the assignment of the buffer object
+
+  // Unbind the buffer object
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  return n;
+}
+
+var g_near = 0.0, g_far = 0.5;
+function keydown(ev, gl, n, u_ProjMatrix, projMatrix, nf) {
+  switch (ev.keyCode) {
+    case 39:
+      g_near += 0.01;
+      break; // 按下右方键
+    case 37:
+      g_near -= 0.01;
+      break; // 按下左方键
+    case 38:
+      g_far += 0.01;
+      break; // 按下上方向键
+    case 40:
+      g_far -= 0.01;
+      break; // 按下下方向键
+    default:
+      return;
+  }
+  draw(gl, n, u_ProjMatrix, projMatrix, nf);
+}
+
+function draw(gl, n, u_ProjMatrix, projMatrix, nf) {
+  projMatrix.setOrtho(-1, 1, -1, 1, g_near, g_far);
+
+  /** 将视图矩阵传递给变量 **/
+  gl.uniformMatrix4fv(u_ProjMatrix, false, projMatrix.elements);
+
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  nf.innerHTML = 'near:' + Math.round(g_near * 100) / 100 + ',far:' + Math.round(g_far * 100) / 100;
+  // 绘制图形
+  gl.drawArrays(gl.TRIANGLES, 0, n);
+}

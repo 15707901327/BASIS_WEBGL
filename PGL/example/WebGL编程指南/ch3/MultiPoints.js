24053058d3b944693384d8b99407e5efc84d8969
15707901327 <1429648501@qq.com>
2019-02-27 17:33:50 +0800

    1.PGL点击绘制多个点

--- PGL/example/WebGL编程指南/ch3/MultiPoints.js
+++ PGL/example/WebGL编程指南/ch3/MultiPoints.js
@@ -5,16 +5,17 @@ window.onload = function (ev) {
 	var webGlRenderer = new PGL.WebGLRenderer({
 		canvas: canvas
 	});
-	webGlRenderer.setClearColor(new PGL.Color(0.5, 1, 1), 1);
+	webGlRenderer.setClearColor(new PGL.Color(0, 0, 0), 1);
 
 	var scene = new PGL.Scene();
 
 	var geometry = new PGL.BufferGeometry();
 	var vertices = [0.0, 0.5, -0.5, -0.5, 0.5, -0.5];
-	geometry.addAttribute('position', new PGL.Float32BufferAttribute(vertices, 3));
+	geometry.addAttribute('position', new PGL.Float32BufferAttribute(vertices, 2));
 
 	var pointsMaterial = new PGL.PointsMaterial({
-		size: 20
+		size: 20,
+		color: new PGL.Color(1, 0, 0)
 	});
 
 	var particles = new PGL.Points(geometry, pointsMaterial);

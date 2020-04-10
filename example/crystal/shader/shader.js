c56bf96e744813cf4bce0045103be17ff9cc38b8
peng <1429648501@qq.com>
2020-04-10 17:32:37 +0800

    1.实现平行光

--- example/crystal/shader/shader.js
+++ example/crystal/shader/shader.js
@@ -2,15 +2,11 @@ export var VERTEX_SHADER = `
 precision mediump float;
 precision mediump int;
 attribute vec4 a_Position;
-attribute vec3 a_Color;
-varying vec3 vColor;
 
 uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
 
 void main() {
-  vColor.xyz = a_Color.xyz;
-
   vec3 transformed = vec3(a_Position);
   vec4 mvPosition = vec4(transformed, 1.0);
   mvPosition = modelViewMatrix * mvPosition;
@@ -19,9 +15,7 @@ void main() {
 `;
 export var FRAGMENT_SHADER = `
 precision mediump float;
-varying vec3 vColor;
 void main(){
-  gl_FragColor = vec4(vColor,1.0);
-  gl_FragColor = vec4(vColor,1.0);
+  gl_FragColor = vec4(0.6,0.6,0.6,1.0);
 }
 `;
